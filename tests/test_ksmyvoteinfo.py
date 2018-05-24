0850e4af152a2a068041323c835813126328cdd9
Peter Karman <karpet@peknet.com>
2018-05-24 12:02:30 -0500

    Fail and Pass tests work

--- tests/test_ksmyvoteinfo.py
+++ tests/test_ksmyvoteinfo.py
@@ -1,11 +1,18 @@
 import pytest
+import re
 from ksmyvoteinfo import KsMyVoteInfo
 
 def test_init():
   assert KsMyVoteInfo()
 
-def test_lookup():
+def test_lookup_fail():
   kmvi = KsMyVoteInfo()
   r = kmvi.lookup(first_name='No', last_name='Such', dob='2000-01-01', county='Douglas')
-  print(r)
-  assert r
+  assert r == False
+
+def test_lookup_pass():
+  kmvi = KsMyVoteInfo()
+  r = kmvi.lookup(first_name='Kris', last_name='Kobach', dob='1966-03-26', county='Douglas')
+  regex = re.compile(r'Republican')
+  matches = regex.search(r)
+  assert matches is not None
