2480ba761ecc088bf761efac9578bbc6a5e78a8f
Peter Karman <karpet@peknet.com>
2018-05-24 11:36:48 -0500

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ksmyvoteinfo',
+      version='0.1',
+      description='Query the KS SOS site for voter registration',
+      url='http://github.com/karpet/ksmyvoteinfo-py',
+      author='Peter Karman',
+      author_email='peter@peknet.com',
+      license='MIT',
+      packages=['ksmyvoteinfo'],
+      zip_safe=False)
